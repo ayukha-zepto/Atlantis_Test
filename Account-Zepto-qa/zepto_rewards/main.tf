c2ea6f19ce8193402fb780e51b3a284c971873ad
ayukha-zepto <133772486+ayukha-zepto@users.noreply.github.com>
2023-08-07 18:30:31 +0530

    Update main.tf

--- Account-Zepto-qa/zepto_rewards/main.tf
+++ Account-Zepto-qa/zepto_rewards/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "zepto_rewards_qa" {
   ami           = "ami-0ad8ef5571e16cef7"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Zepto-rewards-qa"
