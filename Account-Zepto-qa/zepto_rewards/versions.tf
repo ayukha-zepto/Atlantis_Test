79154eb5cf4c9cd5617ff60abd618bbe63a885d2
ayukha-zepto <133772486+ayukha-zepto@users.noreply.github.com>
2023-08-07 18:24:03 +0530

    fixed

--- Account-Zepto-qa/zepto_rewards/versions.tf
+++ Account-Zepto-qa/zepto_rewards/versions.tf
@@ -4,12 +4,14 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.63.0"
-      assume_role {
-        role_arn     = "arn:aws:iam::149309639702:role/qa-atlantis-role"
-        session_name = "${var.atlantis_user}-${var.atlantis_repo_owner}-${var.atlantis_repo_name}-${var.atlantis_pull_num}"
-      }
     }
   }
 
   required_version = "~> 1.4.5"
+}
+provider "aws" {
+  assume_role {
+    role_arn     = "arn:aws:iam::149309639702:role/qa-atlantis-role"
+    session_name = "${var.atlantis_user}-${var.atlantis_repo_owner}-${var.atlantis_repo_name}-${var.atlantis_pull_num}"
+  }
 }
