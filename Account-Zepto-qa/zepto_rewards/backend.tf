96323a66c4c1d59558daa622994e9dc3165df700
ayukha-zepto <133772486+ayukha-zepto@users.noreply.github.com>
2023-08-07 18:17:11 +0530

    fix

--- Account-Zepto-qa/zepto_rewards/backend.tf
+++ Account-Zepto-qa/zepto_rewards/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "zepto-qa-terraform"
     key    = "terraform/atlantis/qa/zepto_rewards/terraform.tfstate"
     region = "ap-south-1"
+    role_arn = "arn:aws:iam::149309639702:role/qa-atlantis-role"
   }
 }
--- Account-Zepto-qa/zepto_rewards/versions.tf
+++ Account-Zepto-qa/zepto_rewards/versions.tf
@@ -1,8 +1,13 @@
 terraform {
   required_providers {
+
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.63.0"
+      assume_role {
+        role_arn     = "arn:aws:iam::149309639702:role/qa-atlantis-role"
+        session_name = "${var.atlantis_user}-${var.atlantis_repo_owner}-${var.atlantis_repo_name}-${var.atlantis_pull_num}"
+      }
     }
   }
 
